9a2a2b06d410978603468de3261307227e0a2382
Samujal Phukan <samujalphukan@yahoo.com>
2026-02-25 16:06:57 +0530

    again hahahhaahhahahah

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,4 +2,48 @@ import { Request, Response } from "express";
 import { registerUser, loginUser} from "./auth.service";
 import { setAuthCookies } from "../../utils/cookies"
 
-export as
+export async function register(
+    req: Request,
+    res: Response
+) {
+    const { name, email, password} = req.body;
+
+    const result = await registerUser(
+        name,
+        email,
+        password
+    );
+
+    setAuthCookies(
+        res,
+        result.accessToken,
+        result.refreshToken
+    );
+
+    res.json({
+        user: result.user
+    })
+}
+
+
+export async function login(
+    req: Request,
+    res: Response
+) {
+    const { email, password } = req.body;
+
+    const result = await loginUser(
+        email,
+        password
+    );
+
+    setAuthCookies(
+        res,
+        result.accessToken,
+        result.refreshToken
+    );
+
+    res.json({
+        user: result.user
+    });
+}
