d56a02d8d3c5f10706f8392234dbf6519b6f8adc
Samujal Phukan <samujalphukan@yahoo.com>
2026-02-25 16:09:05 +0530

    login is done hahahhahaha fuck you

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import cors from "cors";
+import authRoutes from "./modules/auth/auth.routes"
 
 const app = express();
 
@@ -12,5 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use("/auth", authRoutes)
 
 export default app;
