250fbbff07d8e2b2181cedee89c24d5e76e77194
Samujal Phukan <samujalphukan@yahoo.com>
2026-02-26 12:57:44 +0530

    22

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 import { env } from "./env";
 
 export async function connectDB() {
+
+  try {
+
     await mongoose.connect(env.mongoUri);
 
-    console.log("MongoDB connected");
+    console.log("MongoDB Connected");
+
+  } catch (error) {
+
+    console.error("MongoDB Error:", error);
+
+  }
+
 }
