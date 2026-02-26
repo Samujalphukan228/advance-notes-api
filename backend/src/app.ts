9c708ee9f09d52b92fd3fb43dee49cf40575873b
Samujal Phukan <samujalphukan@yahoo.com>
2026-02-26 12:32:09 +0530

    s

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,21 +13,27 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-
  origin: "https://frontend-notes-eight.vercel.app",
-
  credentials: true
-
 }));
 
 app.use(express.json());
-
-app.use(cookieParser());
+app.use(cookieParser();
 
 app.use("/auth", authRoutes);
 app.use("/notes", notesRoutes);
 app.use("/folders", foldersRoutes);
 app.use("/tags", tagsRoutes);
 app.use("/versions", versionsRoutes);
+
+
+/* TEST ROUTE */
+app.get("/test", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "API working",
+    time: new Date()
+  });
+});
 
 export default app;
